aa3c01a7c9bf49113796c8d6d5c83dcc93d3b4f0
sugapriya98 <39941274+sugapriya98@users.noreply.github.com>
2018-06-05 13:29:32 +0530

    Update and rename even.py to vowel.py

--- even.py
+++
@@ -1,9 +0,0 @@
-num=int(input("enter the num"))
-if(num<=100000):
-	if((num%2)==0):
-		print("the num is even")
-	else:
-		print("the num is odd")
-else:
-	print("the num is below one lakh")
-
---
+++ vowel.py
@@ -0,0 +1,6 @@
+er=raw_input("enter the character")
+if(er=='a' or er=='e' or er=='i' or er=='o' or er=='u'):
+	print("the character is vowel")
+else:
+	print("the character is consonant")
+
